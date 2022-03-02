a2272e978ee35d6d634ff716ed10f9a358b4f610
WangZT <wangzitao9@gmail.com>
2022-03-01 22:03:23 -0500

    dnn is finished -- 63%

--- DNN/__init__.py
+++ DNN/__init__.py
@@ -1,9 +1,4 @@
 
-import numpy as np
-from sklearn import svm
-from sklearn.datasets import load_wine
-from sklearn.decomposition import PCA
-from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
@@ -21,12 +16,12 @@ class DeepNeuralNetworks:
 
 
     def mlp_classifier(self, X_train, y_train, X_test, y_test, title):
-        print("人工神经网络分类器", title)
-        # 构建神经网络，其中三个隐藏层，分别有100，50，20个神经元，最大训练次数400
+
+        print("dnn classifier", title)
+
         mlp = MLPClassifier(hidden_layer_sizes=(100, 50, 20), max_iter=400, solver="adam", activation="relu")
-        mlp.fit(X_train, y_train)# 训练
-        predict = mlp.predict(X_test)# 预测
-        # 打印测试结果和真实标签的准确率
+        mlp.fit(X_train, y_train)# training
+        predict = mlp.predict(X_test)# prediction
         print("training rate is :{:.2f}%".format(accuracy_score(y_test, predict) * 100))
         print(classification_report(predict, y_test))
 
